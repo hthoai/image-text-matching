8b27a01d9ed3eb82bca02b11a55de0f970ee9326
hoaiht <hthoai1006@gmail.com>
2021-02-18 10:28:58 +0000

    ADD dataset

--- lib/datasets/precomp_dataset.py
+++ lib/datasets/precomp_dataset.py
@@ -1,5 +1,5 @@
 # -----------------------------------------------------------
-# Stacked Cross Attention Network implementation based on 
+# Stacked Cross Attention Network implementation based on
 # https://arxiv.org/abs/1803.08024.
 # "Stacked Cross Attention for Image-Text Matching"
 # Kuang-Huei Lee, Xi Chen, Gang Hua, Houdong Hu, Xiaodong He
@@ -24,18 +24,18 @@ class PrecompDataset(data.Dataset):
     Possible options: f30k_precomp, coco_precomp
     """
 
-    def __init__(self, data_path, data_split, vocab):
+    def __init__(self, root, split, vocab):
         self.vocab = vocab
-        loc = data_path + '/'
+        loc = root + "/"
 
         # Captions
         self.captions = []
-        with open(loc+'%s_caps.txt' % data_split, 'rb') as f:
+        with open(loc + "%s_caps.txt" % split, "rb") as f:
             for line in f:
                 self.captions.append(line.strip())
 
         # Image features
-        self.images = np.load(loc+'%s_ims.npy' % data_split)
+        self.images = np.load(loc + "%s_ims.npy" % split)
         self.length = len(self.captions)
         # rkiros data has redundancy in images, we divide by 5, 10crop doesn't
         if self.images.shape[0] != self.length:
@@ -43,23 +43,22 @@ class PrecompDataset(data.Dataset):
         else:
             self.im_div = 1
         # the development set for coco is large and so validation would be slow
-        if data_split == 'dev':
+        if split == "dev":
             self.length = 5000
 
     def __getitem__(self, index):
         # handle the image redundancy
-        img_id = index//self.im_div
+        img_id = index // self.im_div
         image = torch.Tensor(self.images[img_id])
         caption = self.captions[index]
         vocab = self.vocab
 
         # Convert caption (string) to word ids.
-        tokens = nltk.tokenize.word_tokenize(
-            str(caption).lower())
+        tokens = nltk.tokenize.word_tokenize(str(caption).lower())
         caption = []
-        caption.append(vocab('<start>'))
+        caption.append(vocab("<start>"))
         caption.extend([vocab(token) for token in tokens])
-        caption.append(vocab('<end>'))
+        caption.append(vocab("<end>"))
         target = torch.Tensor(caption)
         return image, target, index, img_id
 
@@ -96,31 +95,36 @@ def collate_fn(data):
     return images, targets, lengths, ids
 
 
-def get_precomp_loader(data_path, data_split, vocab, opt, batch_size=100,
-                       shuffle=True, num_workers=2):
+def get_precomp_loader(
+    data_path, data_split, vocab, opt, batch_size=100, shuffle=True, num_workers=2
+):
     """Returns torch.utils.data.DataLoader for custom coco dataset."""
     dset = PrecompDataset(data_path, data_split, vocab)
 
-    data_loader = torch.utils.data.DataLoader(dataset=dset,
-                                              batch_size=batch_size,
-                                              shuffle=shuffle,
-                                              pin_memory=True,
-                                              collate_fn=collate_fn)
+    data_loader = torch.utils.data.DataLoader(
+        dataset=dset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        pin_memory=True,
+        collate_fn=collate_fn,
+    )
     return data_loader
 
 
 def get_loaders(data_name, vocab, batch_size, workers, opt):
     dpath = os.path.join(opt.data_path, data_name)
-    train_loader = get_precomp_loader(dpath, 'train', vocab, opt,
-                                      batch_size, True, workers)
-    val_loader = get_precomp_loader(dpath, 'dev', vocab, opt,
-                                    batch_size, False, workers)
+    train_loader = get_precomp_loader(
+        dpath, "train", vocab, opt, batch_size, True, workers
+    )
+    val_loader = get_precomp_loader(
+        dpath, "dev", vocab, opt, batch_size, False, workers
+    )
     return train_loader, val_loader
 
 
-def get_test_loader(split_name, data_name, vocab, batch_size,
-                    workers, opt):
+def get_test_loader(split_name, data_name, vocab, batch_size, workers, opt):
     dpath = os.path.join(opt.data_path, data_name)
-    test_loader = get_precomp_loader(dpath, split_name, vocab, opt,
-                                     batch_size, False, workers)
+    test_loader = get_precomp_loader(
+        dpath, split_name, vocab, opt, batch_size, False, workers
+    )
     return test_loader
