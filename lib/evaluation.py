e6c4783ded95b91a8ce4a315dba659535b953bd8
hoaiht <hthoai1006@gmail.com>
2021-02-27 05:01:18 +0000

    MOD move logger class

--- lib/evaluation.py
+++ lib/evaluation.py
@@ -13,7 +13,6 @@ from __future__ import print_function
 import sys
 import time
 import numpy as np
-from collections import OrderedDict
 
 import torch
 
@@ -22,68 +21,11 @@ from utils.attention import xattn_score_t2i, xattn_score_i2t
 DEVICE = "cuda"
 
 
-class AverageMeter(object):
-    """Computes and stores the average and current value"""
-
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=0):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / (0.0001 + self.count)
-
-    def __str__(self):
-        """String representation for logging"""
-        # for values that should be recorded exactly e.g. iteration number
-        if self.count == 0:
-            return str(self.val)
-        # for stats
-        return "%.4f (%.4f)" % (self.val, self.avg)
-
-
-class LogCollector(object):
-    """A collection of logging objects that can change from train to val"""
-
-    def __init__(self):
-        # to keep the order of logged variables deterministic
-        self.meters = OrderedDict()
-
-    def update(self, k, v, n=0):
-        # create a new meter if previously not recorded
-        if k not in self.meters:
-            self.meters[k] = AverageMeter()
-        self.meters[k].update(v, n)
-
-    def __str__(self):
-        """Concatenate the meters in one log line"""
-        s = ""
-        for i, (k, v) in enumerate(self.meters.items()):
-            if i > 0:
-                s += "  "
-            s += k + " " + str(v)
-        return s
-
-    def tb_log(self, tb_logger, prefix="", step=None):
-        """Log using tensorboard"""
-        for k, v in self.meters.items():
-            tb_logger.log_value(prefix + k, v.val, step=step)
-
-
-def encode_data(model, data_loader, log_step=10, logging=print):
+def encode_data(model, data_loader, exp, log_step=10, logging=print):
     """Encode all images and captions loadable by `data_loader`"""
-    batch_time = AverageMeter()
-    val_logger = LogCollector()
+    batch_time, val_logger = exp.encode_data_start_callback()
 
     # switch to evaluate mode
-    # model.val_start()
     model.eval()
 
     end = time.time()
@@ -123,7 +65,6 @@ def encode_data(model, data_loader, log_step=10, logging=print):
             cap_lens[nid] = cap_len[j]
 
         # measure accuracy and record loss
-        # ?: why do we fw loss here
         model.loss(img_emb, cap_emb, cap_len)
 
         # measure elapsed time
