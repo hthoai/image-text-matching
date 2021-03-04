5c3a99ea1c3585167028bdd19e4a3675df8c505b
hoaiht <hthoai1006@gmail.com>
2021-03-04 07:31:00 +0000

    MOD change dir

--- lib/criterions/triplet_loss.py
+++ lib/criterions/triplet_loss.py
@@ -1,23 +1,27 @@
 from torch import Tensor
 import torch.nn as nn
 
+
 class TripletLoss(nn.Module):
     """
-    Compute contrastive loss
+    Compute triplet loss
     """
 
-    def __init__(
-        self,
-        margin: float
-    ):
+    def __init__(self, margin: float):
         super(TripletLoss, self).__init__()
         self.margin = margin
 
-    def forward(
-        self,
-        scores: Tensor
-    ) -> float:
-        # scores: (No. cap, No. img)
+    def forward(self, scores: Tensor) -> float:
+        """
+        Parameters
+        ----------
+        scores: similarity between all images w.r.t all captions
+        (no. caps, no. imgs)
+
+        Returns
+        -------
+        total_loss: triplet loss
+        """
 
         # get positive score of caption with image
         positive_scores = scores.diag()
@@ -29,7 +33,10 @@ class TripletLoss(nn.Module):
         hardest_neq_images = scores.max(dim=1)
         hardest_neq_captions = scores.max(dim=0)
 
-        loss_captions = (self.margin - positive_scores + hardest_neq_captions).clamp(min=0)
+        loss_captions = (self.margin - positive_scores + hardest_neq_captions).clamp(
+            min=0
+        )
         loss_images = (self.margin - positive_scores + hardest_neq_images).clamp(min=0)
+        total_loss = loss_captions.sum() + loss_images.sum()
 
-        return loss_captions.sum() + loss_images.sum()
+        return total_loss
