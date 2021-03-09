a61838ae9727d333100fef8390357283e2158da8
hoaiht <hthoai1006@gmail.com>
2021-03-09 07:19:58 +0000

    ADD requirements

--- utils/cosine_similarity.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-
-def cosine_similarity(x1, x2, dim=1, eps=1e-8):
-    """Returns cosine similarity between x1 and x2, computed along dim."""
-    w12 = torch.sum(x1 * x2, dim)
-    w1 = torch.norm(x1, 2, dim)
-    w2 = torch.norm(x2, 2, dim)
-    return (w12 / (w1 * w2).clamp(min=eps)).squeeze()
