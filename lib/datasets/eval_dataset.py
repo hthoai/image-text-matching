320db98b5cf5ffa7718172fcb66060584a366fcf
hoaiht <hthoai1006@gmail.com>
2021-03-11 03:50:27 +0000

    MOD read caps only

--- lib/datasets/eval_dataset.py
+++ lib/datasets/eval_dataset.py
@@ -6,8 +6,9 @@ import numpy as np
 
 
 class EvalDataset(data.Dataset):
+    """Read captions only."""
 
-    def __init__(self, root: str, split: str, vocab: Any):
+    def __init__(self, root: str, split: str, vocab: Any, num_imgs: int):
         self.split = split
         self.vocab = vocab
         self.max_length = 0
@@ -22,26 +23,25 @@ class EvalDataset(data.Dataset):
                 self.tokens.append(_tokens)
                 if len(_tokens) > self.max_length:
                     self.max_length = len(_tokens)
-            self.max_length += 2 # for <start>, <end> tokens
+            self.max_length += 2  # for <start>, <end> tokens
 
         # Image features
-        self.images = np.load(loc + "%s_ims.npy" % self.split)
+        # self.images = np.load(loc + "%s_ims.npy" % self.split)
         self.length = len(self.tokens)
         # rkiros data has redundancy in images, we divide by 5, 10crop doesn't
-        if self.images.shape[0] != self.length:
+        if num_imgs != self.length:
             self.im_div = 5
         else:
             self.im_div = 1
         # the development set for coco is large and so validation would be slow
         if split == "dev":
             self.length = 5000
-        self.images = self.images[:self.length]
+        # self.images = self.images[:self.length]
 
-    def __getitem__(self, index):
+    def __getitem__(self, index: int):
         # handle the image redundancy
         img_id = index // self.im_div
         # image = torch.Tensor(self.images[img_id])
-        all_images = torch.Tensor(self.images)
         tokens = self.tokens[index]
         vocab = self.vocab
 
@@ -51,13 +51,13 @@ class EvalDataset(data.Dataset):
         caption.extend([vocab(token) for token in tokens])
         caption.append(vocab("<end>"))
         target = torch.Tensor(caption)
-        return all_images, target, index, img_id
+        return target, index, img_id
 
     def __len__(self):
         return self.length
 
 
-def collate_fn(data):
+def eval_collate_fn(data):
     """Build mini-batch tensors from a list of (image, caption) tuples.
     Args:
         data: list of (image, caption) tuple.
@@ -70,10 +70,8 @@ def collate_fn(data):
         lengths: list; valid length for each padded caption.
     """
     # Sort a data list by caption length
-    # data.sort(key=lambda x: len(x[1]), reverse=True)
-    print(type(data), len(data))
-    print(type(data[0]), len(data[0]))
-    images, captions, ids, img_ids = zip(*data)
+    data.sort(key=lambda x: len(x[0]), reverse=True)
+    captions, ids, img_ids = zip(*data)
 
     # Merge images (convert tuple of 3D tensor to 4D tensor)
     # images = torch.stack(images, 0)
@@ -84,17 +82,7 @@ def collate_fn(data):
     for i, cap in enumerate(captions):
         end = lengths[i]
         targets[i, :end] = cap[:end]
+    lengths = torch.LongTensor(lengths)
 
-    return images, targets, lengths, ids
+    return targets, lengths, ids
 
-
-def get_precomp_loader(dataset, batch_size=128, shuffle=True):
-    """Returns torch.utils.data.DataLoader for custom coco dataset."""
-    data_loader = torch.utils.data.DataLoader(
-        dataset=dataset,
-        batch_size=batch_size,
-        shuffle=shuffle,
-        pin_memory=True,
-        collate_fn=collate_fn,
-    )
-    return data_loader
